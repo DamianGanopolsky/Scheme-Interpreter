1c5cf749ee9f7de6ef8cd34a90918c76649419ca
DamianGanopolsky <damian98dgg@gmail.com>
2021-12-08 14:54:47 -0300

    add func-restar tests

--- test/proyecto_01/core_test.clj
+++ test/proyecto_01/core_test.clj
@@ -46,6 +46,41 @@
 )
 
 
+; user=> (fnc-restar ())
+; (;ERROR: -: Wrong number of args given)
+; user=> (fnc-restar '(3))
+; -3
+; user=> (fnc-restar '(3 4))
+; -1
+; user=> (fnc-restar '(3 4 5))
+; -6
+; user=> (fnc-restar '(3 4 5 6))
+; -12
+; user=> (fnc-restar '(A 4 5 6))
+; (;ERROR: -: Wrong type in arg1 A)
+; user=> (fnc-restar '(3 A 5 6))
+; (;ERROR: -: Wrong type in arg2 A)
+; user=> (fnc-restar '(3 4 A 6))
+; (;ERROR: -: Wrong type in arg2 A)
+
+
+(deftest fnc-restar-test
+
+(testing "Prueba de la funcion: fnc-restar-test"
+(is (= -3 (fnc-restar '(3))))
+(is (= -1 (fnc-restar '(3 4))))
+(is (= -6 (fnc-restar '(3 4 5))))
+(is (= -12 (fnc-restar '(3 4 5 6))))
+)
+(testing "Caso lista vacia")
+(is (= "(;ERROR: -: Wrong number of args given)" (fnc-restar ())))
+
+(testing "Caso tipo incorrecto")
+(is (= "(;ERROR: -: Wrong type in arg1 A)" (fnc-restar '(A 4 5 6))))
+(is (= "(;ERROR: -: Wrong type in arg2 A)" (fnc-restar '(3 A 5 6))))
+(is (= "(;ERROR: -: Wrong type in arg2 A)" (fnc-restar '(3 4 A 6))))
+)
+
 
 
 ;;(deftest saludar-test
